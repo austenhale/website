d69917d4fd75f27c14afa33fbba9cd74092021e9
austenhale <austenphale@gmail.com>
2021-04-14 13:06:01 -0600

    jquery slides

--- a_letter.php
+++ a_letter.php
@@ -1,3 +1,4 @@
+
 <?php 
 session_start();
 $_SESSION['current_page'] = $_SERVER['REQUEST_URI'];
@@ -6,22 +7,25 @@ $_SESSION['comment_table'] = "comments_a";
 <html>
     <head>
         <link rel="stylesheet" href="style.css">
+		
+        <link rel="stylesheet" href="js/a_letter_files/vlb_files1/vlightbox1.css" type="text/css" />
+		<link rel="stylesheet" href="js/a_letter_files/vlb_files1/visuallightbox.css" type="text/css" media="screen" />
+        <script src="js/a_letter_files/vlb_engine/jquery.min.js" type="text/javascript"></script>
+		<script src="js/a_letter_files/vlb_engine/visuallightbox.js" type="text/javascript"></script>
+
        <?php include_once 'animal_header.php'; ?>
     </head>
     <h1 id="animal_name" style="font-family: 'Zen Dots', cursive">African Civet</h1>
     <div id=animal_content>
         <img src="Images/african_civet.jpg" alt="african_civet" id="left_picture">
         
-        <div class="slideshow-container">
-            <div class="mySlides fade">
-                <img src="Images/saharan_africa.png" alt="aouc logo" id="right_picture">
-            </div>
-            <div class="mySlides fade">
-                <img src="Images/saharan_env.jfif" alt="saharan" id="right_picture">
-            </div>
-        </div>
-        
-        <script src="js/slideshow.js"></script>
+
+    <div id="vlightbox1">
+	    <a class="vlightbox1" href="js/a_letter_files/vlb_images1/saharan_env.jpg" title="saharan_env"><img src="js/a_letter_files/vlb_thumbnails1/saharan_env.jpg" alt="saharan_env"/></a>
+        <a class="vlightbox1" href="js/a_letter_files/vlb_images1/saharan_africa.jpg" title="saharan_africa"><img src="js/a_letter_files/vlb_thumbnails1/saharan_africa.jpg" alt="saharan_africa"/></a>
+    <span class="vlb"><a href="http://visuallightbox.com">jquery lightbox with video</a>by VisualLightBox.com v6.1</span>
+	</div>
+	<script src="js/a_letter_files/vlb_engine/vlbdata1.js" type="text/javascript"></script>
         <p id=animal_text>
             The african civet is a nocturnal animal found in Saharan Africa. It will generally
             try to eat anything that it finds; whether it be plants or other animals. It's fur
